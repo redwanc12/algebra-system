daf891b28f196436b02a2e821fd734ffed00a434
redwanchowdhury <redwanc12@gmail.com>
2019-08-17 13:58:06 -0700

    Added all CSS and design componenets.

--- app/src/components/Calculator.vue
+++ app/src/components/Calculator.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="calculator">
 
-    <div class="display">___</div>
-    <div class="btn">Factor</div>
-    <div class="btn">Solve for x</div>
-    <div class="btn">÷</div>
+    <div class="display">0</div>
+    <div class="btn top">AC</div>
+    <div class="btn top">Factor</div>
+    <div class="btn top">Solve</div>
+    <div class="btn op">÷</div>
     <div class="btn">7</div>
     <div class="btn">8</div>
-    <div class="btn">Clear</div>
     <div class="btn">9</div>
-    <div class="btn">x</div>
+    <div class="btn op">x</div>
     <div class="btn">4</div>
     <div class="btn">5</div>
     <div class="btn">6</div>
-    <div class="btn">-</div>
+    <div class="btn op">-</div>
     <div class="btn">1</div>
     <div class="btn">2</div>
     <div class="btn">3</div>
-    <div class="btn">+</div>
+    <div class="btn op">+</div>
     <div class="btn zero">0</div>
     <div class="btn">.</div>
-    <div class="btn">=</div>
+    <div class="btn op">=</div>
   </div>
 </template>
 
@@ -32,20 +32,32 @@ export default {
 
 <style scoped>
 .calculator{
-  font-size: 40px;
+  margin: 0 auto;
+  width: 400px;
+  font-size: 30px;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-auto-rows: minmax(50px, auto);
 }
 .display{
   grid-column: 1 / 5;
+  background-color: #333;
+  color: white;
 }
 .zero{
   grid-column: 1 / 3;
 }
 .btn {
-  background-color: #eee;
-  border: 1px solid #333
+  background-color: #777;
+  color: white;
+  border: 0.5px solid black;
+}
+.op {
+  background-color: orange;
+  color: white;
+}
+.top{
+  background-color: #555;
 }
 
 </style>
